911575e7e9531f51a568cc3ab1300bd9454b948b
Max Schrementi <dwolrdcojp@gmail.com>
2020-12-20 20:15:10 -0800

    Add program to create histogram on word lengths

---
+++ word_counting/histogram.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\n' || c == '\t' || c == ' ')
+      putchar('\n');
+    else {
+      putchar('|');
+    }
+  }
+}
